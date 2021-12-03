bc61948c175952eda051d5c92108f84d0dfbad78
lts5-ts <thomas.steg87@gmail.com>
2021-12-03 14:44:43 +0100

    loop

--- script_loop_train.py
+++ script_loop_train.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # In[ ]:
-
+import os
 
 data = ['cambodge']
 AGEs = [False,True]
@@ -14,5 +14,5 @@ for data in data:
     for AGE in AGEs:
         for SEXE in SEXEs:
             for size in IMG_SIZEs:
-                get_ipython().system("python demo_bmai.py --model_name 'mobilenet' --data_name data --img_size size --SEXE SEXE --AGE AGE --epochs 20 --lr 0.005 --batch_size 128 --num_workers 16")
+                os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data} --img_size {size} --SEXE {SEXE} --AGE {AGE} --epochs 20 --lr 0.005 --batch_size 128 --num_workers 16')
 
