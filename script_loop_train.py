064eb6beacc6e64f4075f39da0ab3615da846c3d
Ahmed Ben Haj Yahia <ahmed.benhajyahia@epfl.ch>
2022-01-04 12:28:51 +0100

    Update script_loop_train.py

--- script_loop_train.py
+++ script_loop_train.py
@@ -24,7 +24,7 @@ SEXEs =['False','True']
 
 IMG_SIZEs = [256,384]
 
-for data_name in data:
+for data_name in data[1:]:
     for img_size in IMG_SIZEs[1:]:
         for AGE in AGEs[1:]:
             for SEXE in SEXEs[1:]:
@@ -34,4 +34,4 @@ for data_name in data:
                         os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data_name} --SEED {SEED} --img_size {img_size} --SEXE {SEXE} --AGE {AGE} --method_sex_age 4 --epochs {epochs} --lr 0.005 --batch_size 32 --num_workers 16')
                     else:
                         epochs=30
-                        os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data_name} --SEED {SEED} --img_size {img_size} --SEXE {SEXE} --AGE {AGE} --method_sex_age 4 --epochs {epochs} --lr 0.005 --batch_size 32 --num_workers 16')
+                        os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data_name} --SEED {SEED} --img_size {img_size} --SEXE {SEXE} --AGE {AGE} --method_sex_age 4 --epochs {epochs} --lr 0.005 --batch_size 24 --num_workers 16')
