3b6f0e7f0a6fa786212f70a256bd7b83318a4d48
Ahmed Ben Haj Yahia <ahmed.benhajyahia@epfl.ch>
2022-01-04 03:46:29 +0100

    aaa

--- script_loop_train.py
+++ script_loop_train.py
@@ -5,17 +5,17 @@
 import os
 import pandas as pd
 
-# pd.DataFrame(columns=[
-#        'data',
-#        'img_size',
-#        'sexe',
-#        'age',
-#        'method_sex_age',
-#        'seed',
-#        'epochs',
-#        'lr',
-#        'height_rel_err',
-#        'weight_rel_err']).to_csv('results/mobilenet_v2_results_guinee_new_method.csv',index=False)
+pd.DataFrame(columns=[
+       'data',
+       'img_size',
+       'sexe',
+       'age',
+       'method_sex_age',
+       'seed',
+       'epochs',
+       'lr',
+       'height_rel_err',
+       'weight_rel_err']).to_csv('results/mobilenet_v2_results_with_age_sexe_branch.csv',index=False)
 
 
 data = ['guinee','cambodge','guinee_cambodge']
@@ -24,9 +24,14 @@ SEXEs =['False','True']
 
 IMG_SIZEs = [256,384]
 
-for data_name in data[1:2]:
+for data_name in data:
     for img_size in IMG_SIZEs[1:]:
         for AGE in AGEs[1:]:
             for SEXE in SEXEs[1:]:
-                for SEED in [0,1]:#,1,2,3]:
-                    os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data_name} --SEED {SEED} --img_size {img_size} --SEXE {SEXE} --AGE {AGE} --method_sex_age 4 --epochs 30 --lr 0.005 --batch_size 128 --num_workers 16')
+                for SEED in [0,1,2,3]:
+                    if data=='guinee':
+                        epochs=20
+                        os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data_name} --SEED {SEED} --img_size {img_size} --SEXE {SEXE} --AGE {AGE} --method_sex_age 4 --epochs {epochs} --lr 0.005 --batch_size 128 --num_workers 16')
+                    else:
+                        epochs=30
+                        os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data_name} --SEED {SEED} --img_size {img_size} --SEXE {SEXE} --AGE {AGE} --method_sex_age 4 --epochs {epochs} --lr 0.005 --batch_size 128 --num_workers 16')
