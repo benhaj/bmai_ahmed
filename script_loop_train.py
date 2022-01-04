942861f30c3cd7bc57decfb7174dacb30612a577
Ahmed Ben Haj Yahia <ahmed.benhajyahia@epfl.ch>
2022-01-04 03:47:31 +0100

    Update script_loop_train.py

--- script_loop_train.py
+++ script_loop_train.py
@@ -29,7 +29,7 @@ for data_name in data:
         for AGE in AGEs[1:]:
             for SEXE in SEXEs[1:]:
                 for SEED in [0,1,2,3]:
-                    if data=='guinee':
+                    if data_name=='guinee':
                         epochs=20
                         os.system(f'python demo_bmai.py --model_name mobilenet --data_name {data_name} --SEED {SEED} --img_size {img_size} --SEXE {SEXE} --AGE {AGE} --method_sex_age 4 --epochs {epochs} --lr 0.005 --batch_size 128 --num_workers 16')
                     else:
