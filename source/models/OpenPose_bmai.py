fa4ee1f3ab8cb70aa5b6c7111055c14cc3dce227
Ahmed Ben Haj Yahia <ahmed.benhajyahia@epfl.ch>
2022-02-03 14:42:08 +0100

    Update OpenPose_bmai.py

--- source/models/OpenPose_bmai.py
+++ source/models/OpenPose_bmai.py
@@ -244,7 +244,7 @@ class InitialStage(nn.Module):
         pafs = self.pafs(trunk_features)
 #         return [heatmaps, pafs]
         print(pafs.shape)
-        return np.sum(pafs,axis=2)
+        return torch.sum(pafs,axis=2)
 
 class RefinementStageBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
